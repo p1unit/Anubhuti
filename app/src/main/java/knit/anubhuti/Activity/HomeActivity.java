25fb1ccfc3c95da172b2632898578b18db358c92
Puneet kumar <puneet16638@gmail.com>
2018-11-11 23:08:22 +0530

    home Page created

---
+++ app/src/main/java/knit/anubhuti/Activity/HomeActivity.java
@@ -0,0 +1,17 @@
+package knit.anubhuti.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import knit.anubhuti.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
